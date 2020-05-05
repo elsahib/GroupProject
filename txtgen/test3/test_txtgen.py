1e01533e175549ba09f4116bfbe3f6bc65420d68
Marbul83 <markbulfin@gmail.com>
2020-05-05 20:30:04 +0100

    working testing added

--- txtgen/test3/test_txtgen.py
+++ txtgen/test3/test_txtgen.py
@@ -7,18 +7,11 @@ from application import app
 class TestBase(TestCase):
 
     def create_app(self):
-
-        # pass in configurations for test database
         config_name = 'testing'
         return app
 
 class TestViews(TestBase):
 
     def test_text(self):
-        """
-        Test that text is generated
-        """
         response = self.client.get(url_for('text'))
         self.assertEqual(response.status_code, 200)
-
-
